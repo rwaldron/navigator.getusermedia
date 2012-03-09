b3d4752fcea27a5a8656b0191325705ba0da5abb
Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>
2012-03-09 10:42:02 -0500

    Move lib/gum.js => src/navigator.getusermedia.js; Preserves commit history
    Signed-off-by: Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>

--- src/navigator.getusermedia.js
+++ src/navigator.getusermedia.js
@@ -1,28 +1,55 @@
-/*
- * navigator.getusermedia
- * https://github.com/rick/navigator.getusermedia
- *
- * Copyright (c) 2012 Rick Waldron <waldron.rick@gmail.com>
- * Licensed under the MIT license.
- */
+/* gum - v0.1.0 - 3/08/2012
+* https://github.com/rwldrn/dmv (gum)
+* Copyright (c) 2012 Rick Waldron <waldron.rick@gmail.com>; Licensed MIT */
+(function( window, navigator ) {
+  // 2012-03-08 Inspired by https://gist.github.com/f2ac64ed7fc467ccdfe3
 
-(function($) {
+  // If unprefix.js is available, use it.
+  // https://github.com/rwldrn/unprefix.js
+  // Otherwise...
+  if ( !window.unprefix ) {
+    // Thanks to Mike Taylr for typing this
+    // https://gist.github.com/f2ac64ed7fc467ccdfe3
+    // normalize window.URL
+    if ( !window.URL ) {
+      window.URL = window.webkitURL || window.msURL || window.oURL;
+    }
+    // normalize navigator.getUserMedia
+    if ( !navigator.getUserMedia ) {
+      navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    }
+  }
 
-  // Collection method.
-  $.fn.awesome = function() {
-    return this.each(function() {
-      $(this).html('awesome');
-    });
+  var spec = true,
+      getUserMedia = navigator.getUserMedia;
+
+  try {
+    navigator.getUserMedia({ video: true, audio: true }, function() {});
+  } catch(e) {
+    spec = false;
+  }
+
+  navigator.getUserMedia = function( opts, callback, errback ) {
+    var options = opts,
+        // Create guard against bogus options
+        safe = { video: 1, audio: 1 };
+
+    if ( !spec ) {
+      options = Object.keys( opts ).filter(function( key ) {
+        return this[ key ] && safe[ key ];
+      }, opts ).join(",");
+    }
+
+    getUserMedia.call( navigator, options, function( stream ) {
+      //  Standard stream
+      if ( stream.label && stream.readyState === 1 ) {
+        stream = window.URL.createObjectURL( stream );
+      }
+
+      callback( stream );
+    }, errback || function() {});
   };
 
-  // Static method.
-  $.awesome = function() {
-    return 'awesome';
-  };
+  window.navigator = navigator;
 
-  // Custom selector.
-  $.expr[':'].awesome = function(elem) {
-    return elem.textContent.indexOf('awesome') >= 0;
-  };
-
-}(jQuery));
+} (typeof window === "object" && window || this, this.navigator || {} ) );
