93fd1b0c40b9b803ef43540730d88f015267a06f
Rick Waldron <waldron.rick@gmail.com>
2013-01-04 14:16:21 -0500

    adds node_modules to gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+    /*global config:true, task:true, process:true*/
+module.exports = function( grunt ) {
+
+  grunt.initConfig({
+    pkg: "<json:package.json>",
+    concat: {
+      "dist/navigator.getusermedia.js": ["<file_strip_banner:src/navigator.getusermedia.js>"]
+    },
+    uglify: {
+      "dist/navigator.getusermedia.min.js": ["dist/navigator.getusermedia.js"]
+    },
+    qunit: {
+      files: ["test/**/*.html"]
+    },
+    watch: {
+      files: "<config:lint.files>",
+      tasks: "lint qunit"
+    },
+    jshint: {
+      files: ["grunt.js", "src/**/*.js", "test/**/*.js"]
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-qunit");
+
+  // Default grunt
+  grunt.registerTask( "default", [ "jshint", "uglify", "qunit" ] );
+
+};
